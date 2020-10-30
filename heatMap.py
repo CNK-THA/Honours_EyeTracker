51e4f5b402802c9df658097aead5c875768cdc0d
CNK-THA <47205250+CNK-THA@users.noreply.github.com>
2020-10-30 13:53:46 +1000

    Demo update

--- heatMap.py
+++ heatMap.py
@@ -23,64 +23,6 @@ with open('GazeReading.csv') as csvFile:
             y = []
 
 
-#                        First Image                    Second Image
-#data contains [ [[x1, x2, x3], [y1, y2, y3]], [[x1, x2, x3], [y1, y2, y3]] ... ]
-
-print('hehehe')
-##print(data)
-
-            
-#DEFINE GRID SIZE AND RADIUS(h)
-# grid_size=1
-# h=1
-#
-# #GETTING X,Y MIN AND MAX
-# x_min=min(x)
-# x_max=max(x)
-# y_min=min(y)
-# y_max=max(y)
-#
-# #CONSTRUCT GRID
-# x_grid=np.arange(x_min-h,x_max+h,grid_size)
-# y_grid=np.arange(y_min-h,y_max+h,grid_size)
-# x_mesh,y_mesh=np.meshgrid(x_grid,y_grid)
-#
-# #GRID CENTER POINT
-# xc=x_mesh+(grid_size/2)
-# yc=y_mesh+(grid_size/2)
-#
-# #FUNCTION TO CALCULATE INTENSITY WITH QUARTIC KERNEL
-# def kde_quartic(d,h):
-#     dn=d/h
-#     P=(15/16)*(1-dn**2)**2
-#     return P
-#
-# #PROCESSING
-# intensity_list=[]
-# for j in range(len(xc)):
-#     intensity_row=[]
-#     for k in range(len(xc[0])):
-#         kde_value_list=[]
-#         for i in range(len(x)):
-#             #CALCULATE DISTANCE
-#             d=math.sqrt((xc[j][k]-x[i])**2+(yc[j][k]-y[i])**2)
-#             if d<=h:
-#                 p=kde_quartic(d,h)
-#             else:
-#                 p=0
-#             kde_value_list.append(p)
-#         #SUM ALL INTENSITY VALUE
-#         p_total=sum(kde_value_list)
-#         intensity_row.append(p_total)
-#     intensity_list.append(intensity_row)
-
-#HEATMAP OUTPUT    
-# intensity=np.array(intensity_list)
-# plt.pcolormesh(x_mesh,y_mesh,intensity)
-# plt.plot(x,y,'ro')
-# plt.colorbar()
-# plt.show()
-
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -90,12 +32,7 @@ import os
 import glob
 ##https://www.expyriment.org/
 
-# UNUSED
-# x = np.random.rayleigh(50, size=5000)
-# y = np.random.rayleigh(50, size=5000)
-# print(len(x))
-# plt.hist2d(x,y, bins=[np.arange(0,400,5),np.arange(0,300,5)])
-# fig, axl = plt.subplots(figsize=(20,20))
+
 
 dir_path = os.path.dirname(os.path.realpath(__file__)) + '/Phishing emails/*.*' #forward slashes for Linux directory, backward slashes for windows
 image_files = glob.glob(dir_path)
@@ -105,10 +42,6 @@ countIndex = 0
 x = None
 y = None
 
-##print('wer')
-##
-##print(dir_path)
-##print(image_files)
 
 
 for image in image_files:
@@ -123,25 +56,14 @@ for image in image_files:
     y_max = max(y)
     print('max x', x_max)
     print('max y', y_max)
-##    reScale = (rescale[0]/1, rescale[1]/1)
-##    print(reScale)
+
     print(reScale1, 'image size')
 
-
-    
-##    r = 0
     for count in range (0,len(x)):
-##        print(x[count], y[count])
+
         x[count] = x[count] * reScale1[0]
         y[count] = y[count] * reScale1[1]
-##        print('becomes', x[count], y[count])
-##        r+=1
-##        if( r == 40):
-##            break
-##    break
 
-
-# img2 = img.resize((np.array(img.size)/10).astype(int)) UNUSED
 
 
 
